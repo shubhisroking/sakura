679e1d07632d77b4f0ad160a499bf75b6907d201
Shubh <shubhisroking@proton.me>
2025-08-03 03:14:13 +0530

    if ain't bussin we ain't fussin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,12 @@
 import { createAuthClient } from "better-auth/react"
+
 export const authClient = createAuthClient({
-    
-    /** The base URL of the server (optional if you're using the same domain) */
     baseURL: process.env.BETTER_AUTH_BASE_URL || "http://localhost:3000",
 })
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const { signIn, signUp, useSession } = authClient
+
+export const signInWithSlack = async () => {
+  const data = await authClient.signIn.social({ provider: "slack" });
+  return data;
+};
